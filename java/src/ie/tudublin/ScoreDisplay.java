5f8fe41e8c56a6c7de2cb76f087034a420621c3c
Stephen Halligan <sldhalligan@gmail.com>
2021-04-21 21:22:26 +0100

    Added some notes

--- java/src/ie/tudublin/ScoreDisplay.java
+++ java/src/ie/tudublin/ScoreDisplay.java
@@ -7,8 +7,8 @@ import processing.core.PApplet;
 
 public class ScoreDisplay extends PApplet {
 
-	//String score = "DEFGABcd";
-	String score = "D1E2F2G2A2B2c2d2";
+	String score = "DEFGABcd";
+	//String score = "D2E2F2G2A2B2c2d2";
 	//String score = "DEF2F2F2EFA2A2B2AFD2E2D2D2D2";
 
 	ArrayList <Note> noteList = new ArrayList<Note>();
@@ -21,7 +21,7 @@ public class ScoreDisplay extends PApplet {
 			int duration = 1;
 
 			if(i+1 != score.length()) {
-				if(score.charAt(i+1) == '2' && i < score.length()) {
+				if(score.charAt(i+1) == '2') {
 					duration = 2;
 				}
 				else {
@@ -32,7 +32,7 @@ public class ScoreDisplay extends PApplet {
 			Note note = new Note(score.charAt(i), duration);
 			noteList.add(note);
 
-			if(score.charAt(i) == '2') {
+			if(score.charAt(i) == '2' || score.charAt(i) == '1') {
 				noteList.remove(note);
 			}
 
@@ -41,10 +41,20 @@ public class ScoreDisplay extends PApplet {
 
 	public void printScores() {
 
-		String typeOfNote = "Quaver";
-		for(Note note : noteList) {
-			System.out.println(note.toString()+" "+typeOfNote);
+		String typeOfNote;
 
+		typeOfNote = "Quaver";
+		for(Note note : noteList) {
+
+			if(note.getDuration() == 2) {
+				typeOfNote = "Crotchet";
+			}
+			if(note.getDuration() == 1) {
+				typeOfNote = "Quaver";
+			}
+
+			System.out.println(note.toString() + " " + typeOfNote);
+			
 		}
 	}
 
@@ -63,8 +73,6 @@ public class ScoreDisplay extends PApplet {
 		loadScore();
 		printScores();
 
-		drawNotes();
-
 	}
 
 	public void draw() {
@@ -72,6 +80,8 @@ public class ScoreDisplay extends PApplet {
 		background(255);
 		
 		drawStave();
+
+		drawNotes();
 
 	}
 
@@ -89,41 +99,100 @@ public class ScoreDisplay extends PApplet {
 
 	void drawNotes() {
 
+		float xPos = width/2-width/3f;
+		int size = 22;
 		for(int i = 0; i < noteList.size(); i++) {
-			//Note note = new Note(score.charAt(i), 1);
-			//System.out.println(noteList.get(i).toString());
 
-			String currNote = noteList.get(i).toString().substring(0, noteList.get(i).toString().indexOf(" ")); 
+			switch(noteList.get(i).toString()) {
+				case "D 2":
+					ellipse(xPos, height/2-height/5+112.5f, size, size);
+					fill(0);
+					line(xPos+size/2, height/2-height/5+112.5f, xPos+size/2, height/2-height/3+112.5f);
+					break;
+				case "E 2":
+					ellipse(xPos, height/2-height/5+100f, size, size);
+					fill(0);
+					line(xPos+size/2, height/2-height/5+100, xPos+size/2, height/2-height/3+100);
+					break;
+				case "F 2":
+					ellipse(xPos, height/2-height/5+87.5f, size, size);
+					fill(0);
+					line(xPos+size/2, height/2-height/5+87.5f, xPos+size/2, height/2-height/3+87.5f);
+					break;
+				case "G 2":
+					ellipse(xPos, height/2-height/5+75f, size, size);
+					fill(0);
+					line(xPos+size/2, height/2-height/5+75f, xPos+size/2, height/2-height/3+75f);
+					break;
+				case "A 2":
+					ellipse(xPos, height/2-height/5+62.5f, size, size);
+					fill(0);
+					line(xPos+size/2, height/2-height/5+62.5f, xPos+size/2, height/2-height/3+62.5f);
+					break;
+				case "B 2":
+					ellipse(xPos, height/2-height/5+50f, size, size);
+					fill(0);
+					line(xPos+size/2, height/2-height/5+50f, xPos+size/2, height/2-height/3+50f);
+					break;
+				case "c 2":
+					ellipse(xPos, height/2-height/5+37.5f, size, size);
+					fill(0);
+					line(xPos+size/2, height/2-height/5+37.5f, xPos+size/2, height/2-height/3+37.5f);
+					break;
+				case "d 2":
+					ellipse(xPos, height/2-height/5+25f, size, size);
+					fill(0);
+					line(xPos+size/2, height/2-height/5+25f, xPos+size/2, height/2-height/3+25f);
+					break;
 
-			/*switch(currNote) {
-				case "D":
-					System.out.println("WORKIN");
+
+				case "D 1":
+					ellipse(xPos, height/2-height/5+112.5f, size, size);
+					fill(0);
+					line(xPos+size/2, height/2-height/5+112.5f, xPos+size/2, height/2-height/3+112.5f);
+					line(xPos+size/2, height/2-height/3+112.5f, xPos+size/2+15, height/2-height/3+125f);
 					break;
 				case "E 1":
-					System.out.println("f");
+					ellipse(xPos, height/2-height/5+100f, size, size);
+					fill(0);
+					line(xPos+size/2, height/2-height/5+100, xPos+size/2, height/2-height/3+100);
+					line(xPos+size/2, height/2-height/3+100, xPos+size/2+15, height/2-height/3+112.5f);
 					break;
 				case "F 1":
-					System.out.println("f");
+					ellipse(xPos, height/2-height/5+87.5f, size, size);
+					fill(0);
+					line(xPos+size/2, height/2-height/5+87.5f, xPos+size/2, height/2-height/3+87.5f);
 					break;
 				case "G 1":
-					System.out.println("f");
+					ellipse(xPos, height/2-height/5+75f, size, size);
+					fill(0);
+					line(xPos+size/2, height/2-height/5+75f, xPos+size/2, height/2-height/3+75f);
 					break;
 				case "A 1":
-					System.out.println("f");
+					ellipse(xPos, height/2-height/5+62.5f, size, size);
+					fill(0);
+					line(xPos+size/2, height/2-height/5+62.5f, xPos+size/2, height/2-height/3+62.5f);
 					break;
 				case "B 1":
-					System.out.println("f");
+					ellipse(xPos, height/2-height/5+50f, size, size);
+					fill(0);
+					line(xPos+size/2, height/2-height/5+50f, xPos+size/2, height/2-height/3+50f);
 					break;
 				case "c 1":
-					System.out.println("f");
+					ellipse(xPos, height/2-height/5+37.5f, size, size);
+					fill(0);
+					line(xPos+size/2, height/2-height/5+37.5f, xPos+size/2, height/2-height/3+37.5f);
 					break;
 				case "d 1":
-					System.out.println("f");
+					ellipse(xPos, height/2-height/5+25f, size, size);
+					fill(0);
+					line(xPos+size/2, height/2-height/5+25f, xPos+size/2, height/2-height/3+25f);
 					break;
 
 				
 
-			}*/
+			}
+			xPos = xPos + 100;
 		}
 
 	}
