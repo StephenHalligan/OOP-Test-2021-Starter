b3506b2b9a10569519a34afcbf86e5e6808e94db
Stephen Halligan <sldhalligan@gmail.com>
2021-04-21 21:39:54 +0100

    Made it so notes change to red when mouse is over them

--- java/src/ie/tudublin/ScoreDisplay.java
+++ java/src/ie/tudublin/ScoreDisplay.java
@@ -89,6 +89,9 @@ public class ScoreDisplay extends PApplet {
 	
 		strokeWeight(3);
 
+		fill(0);
+		stroke(0);
+
 		line(width/2-width/2.2f, height/2-height/5, width/2+width/2.2f, height/2-height/5); 			//F
 		line(width/2-width/2.2f, height/2-height/5+25, width/2+width/2.2f, height/2-height/5+25);   	//D
 		line(width/2-width/2.2f, height/2-height/5+50, width/2+width/2.2f, height/2-height/5+50);  	 	//Middle line (B)
@@ -101,117 +104,112 @@ public class ScoreDisplay extends PApplet {
 
 		float xPos = width/2-width/3f;
 		int size = 22;
+
 		for(int i = 0; i < noteList.size(); i++) {
+
+			fill(0);
 			textSize(26);
+
+			if(mouseX > xPos - 50 && mouseX < xPos + 50) {
+				fill(255, 0, 0);
+				stroke(255, 0, 0);
+			}
+			else {
+				fill(0);
+				stroke(0);
+			}
+
 			switch(noteList.get(i).toString()) {
 				case "D 2":
 					ellipse(xPos, height/2-height/5+112.5f, size, size);
-					fill(0);
 					line(xPos+size/2, height/2-height/5+112.5f, xPos+size/2, height/2-height/3+112.5f);
 					text("D", xPos, height/2-height/3);
 					break;
 				case "E 2":
 					ellipse(xPos, height/2-height/5+100f, size, size);
-					fill(0);
 					line(xPos+size/2, height/2-height/5+100, xPos+size/2, height/2-height/3+100);
-					text("E", xPos, height/2-height/4);
+					text("E", xPos, height/2-height/3);
 					break;
 				case "F 2":
 					ellipse(xPos, height/2-height/5+87.5f, size, size);
-					fill(0);
 					line(xPos+size/2, height/2-height/5+87.5f, xPos+size/2, height/2-height/3+87.5f);
-					text("F", xPos, height/2-height/4);
+					text("F", xPos, height/2-height/3);
 					break;
 				case "G 2":
 					ellipse(xPos, height/2-height/5+75f, size, size);
-					fill(0);
 					line(xPos+size/2, height/2-height/5+75f, xPos+size/2, height/2-height/3+75f);
-					text("G", xPos, height/2-height/4);
+					text("G", xPos, height/2-height/3);
 					break;
 				case "A 2":
 					ellipse(xPos, height/2-height/5+62.5f, size, size);
-					fill(0);
 					line(xPos+size/2, height/2-height/5+62.5f, xPos+size/2, height/2-height/3+62.5f);
-					text("A", xPos, height/2-height/4);
+					text("A", xPos, height/2-height/3);
 					break;
 				case "B 2":
 					ellipse(xPos, height/2-height/5+50f, size, size);
-					fill(0);
 					line(xPos+size/2, height/2-height/5+50f, xPos+size/2, height/2-height/3+50f);
-					text("B", xPos, height/2-height/4);
+					text("B", xPos, height/2-height/3);
 					break;
 				case "c 2":
 					ellipse(xPos, height/2-height/5+37.5f, size, size);
-					fill(0);
 					line(xPos+size/2, height/2-height/5+37.5f, xPos+size/2, height/2-height/3+37.5f);
-					text("c", xPos, height/2-height/4);
+					text("c", xPos, height/2-height/3);
 					break;
 				case "d 2":
 					ellipse(xPos, height/2-height/5+25f, size, size);
-					fill(0);
 					line(xPos+size/2, height/2-height/5+25f, xPos+size/2, height/2-height/3+25f);
-					text("d", xPos, height/2-height/4);
+					text("d", xPos, height/2-height/3);
 					break;
 
 
 				case "D 1":
 					ellipse(xPos, height/2-height/5+112.5f, size, size);
-					fill(0);
 					line(xPos+size/2, height/2-height/5+112.5f, xPos+size/2, height/2-height/3+112.5f);
 					line(xPos+size/2, height/2-height/3+112.5f, xPos+size/2+15, height/2-height/3+125f);
-					text("D", xPos, height/2-height/4);
+					text("D", xPos, height/2-height/3);
 					break;
 				case "E 1":
 					ellipse(xPos, height/2-height/5+100f, size, size);
-					fill(0);
 					line(xPos+size/2, height/2-height/5+100, xPos+size/2, height/2-height/3+100);
 					line(xPos+size/2, height/2-height/3+100, xPos+size/2+15, height/2-height/3+112.5f);
-					text("E", xPos, height/2-height/4);
+					text("E", xPos, height/2-height/3);
 					break;
 				case "F 1":
 					ellipse(xPos, height/2-height/5+87.5f, size, size);
-					fill(0);
 					line(xPos+size/2, height/2-height/5+87.5f, xPos+size/2, height/2-height/3+87.5f);
 					line(xPos+size/2, height/2-height/3+87.5f, xPos+size/2+15, height/2-height/3+100f);
-					text("F", xPos, height/2-height/4);
+					text("F", xPos, height/2-height/3);
 					break;
 				case "G 1":
 					ellipse(xPos, height/2-height/5+75f, size, size);
-					fill(0);
 					line(xPos+size/2, height/2-height/5+75f, xPos+size/2, height/2-height/3+75f);
 					line(xPos+size/2, height/2-height/3+75, xPos+size/2+15, height/2-height/3+87.5f);
-					text("G", xPos, height/2-height/4);
+					text("G", xPos, height/2-height/3);
 					break;
 				case "A 1":
 					ellipse(xPos, height/2-height/5+62.5f, size, size);
-					fill(0);
 					line(xPos+size/2, height/2-height/5+62.5f, xPos+size/2, height/2-height/3+62.5f);
 					line(xPos+size/2, height/2-height/3+62.5f, xPos+size/2+15, height/2-height/3+75);
-					text("A", xPos, height/2-height/4);
+					text("A", xPos, height/2-height/3);
 					break;
 				case "B 1":
 					ellipse(xPos, height/2-height/5+50f, size, size);
-					fill(0);
 					line(xPos+size/2, height/2-height/5+50f, xPos+size/2, height/2-height/3+50f);
 					line(xPos+size/2, height/2-height/3+50, xPos+size/2+15, height/2-height/3+62.5f);
-					text("B", xPos, height/2-height/4);
+					text("B", xPos, height/2-height/3);
 					break;
 				case "c 1":
 					ellipse(xPos, height/2-height/5+37.5f, size, size);
-					fill(0);
 					line(xPos+size/2, height/2-height/5+37.5f, xPos+size/2, height/2-height/3+37.5f);
 					line(xPos+size/2, height/2-height/3+37.5f, xPos+size/2+15, height/2-height/3+50);
-					text("c", xPos, height/2-height/4);
+					text("c", xPos, height/2-height/3);
 					break;
 				case "d 1":
 					ellipse(xPos, height/2-height/5+25f, size, size);
-					fill(0);
 					line(xPos+size/2, height/2-height/5+25f, xPos+size/2, height/2-height/3+25f);
 					line(xPos+size/2, height/2-height/3+25, xPos+size/2+15, height/2-height/3+37.5f);
-					text("d", xPos, height/2-height/4);
+					text("d", xPos, height/2-height/3);
 					break;
-
-				
 
 			}
 			xPos = xPos + 100;
