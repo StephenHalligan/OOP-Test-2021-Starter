0325faf3e1bdf16f5abc288a39dfaf56507a2303
Stephen Halligan <sldhalligan@gmail.com>
2021-04-21 18:37:56 +0100

    Added drawStave method to draw the 5 stave lines

--- java/src/ie/tudublin/Note.java
+++ java/src/ie/tudublin/Note.java
@@ -6,12 +6,16 @@ public class Note
     private int duration;
 
     public Note(char note, int duration) {
+
         this.note = note;
         this.duration = duration;
+
     }
 
     public String toString() {
+
         return note+ " " +duration;  
+        
     }
 
 
--- java/src/ie/tudublin/ScoreDisplay.java
+++ java/src/ie/tudublin/ScoreDisplay.java
@@ -5,8 +5,8 @@ import java.util.ArrayList;
 
 import processing.core.PApplet;
 
-public class ScoreDisplay extends PApplet
-{
+public class ScoreDisplay extends PApplet {
+	
 	String score = "DEFGABcd";
 	//String score = "D2E2F2G2A2B2c2d2";
 	//String score = "DEF2F2F2EFA2A2B2AFD2E2D2D2D2";
@@ -14,9 +14,11 @@ public class ScoreDisplay extends PApplet
 	ArrayList <Note> noteList = new ArrayList<Note>();
 
 	public void loadScore() {
+
 		for(int i = 0; i < score.length(); i++) {
 			Note note = new Note(score.charAt(i), 1);
 			noteList.add(note);
+
 		}
 	}
 
@@ -25,11 +27,12 @@ public class ScoreDisplay extends PApplet
 		String typeOfNote = "Quaver";
 		for(Note note : noteList) {
 			System.out.println(note.toString()+" "+typeOfNote);
+
 		}
 	}
 
-	public void settings()
-	{
+	public void settings() {
+
 		size(1000, 500);
 
 		// How to convert a character to a number
@@ -38,19 +41,36 @@ public class ScoreDisplay extends PApplet
 		println(i);
 	}
 
-	public void setup() 
-	{
+	public void setup() {
+
 		loadScore();
 		printScores();
-	}
-
-	public void draw()
-	{
-		background(255);	
-	}
-
-	void drawNotes()
-	{
 
 	}
+
+	public void draw() {
+
+		background(255);
+		
+		drawStave();
+
+	}
+
+	void drawStave() {
+	
+		strokeWeight(3);
+
+		line(width/2-width/2.2f, height/2-height/5, width/2+width/2.2f, height/2-height/5); 			//F
+		line(width/2-width/2.2f, height/2-height/5+25, width/2+width/2.2f, height/2-height/5+25);   	//D
+		line(width/2-width/2.2f, height/2-height/5+50, width/2+width/2.2f, height/2-height/5+50);  	 	//Middle line (B)
+		line(width/2-width/2.2f, height/2-height/5+75, width/2+width/2.2f, height/2-height/5+75);   	//G
+		line(width/2-width/2.2f, height/2-height/5+100, width/2+width/2.2f, height/2-height/5+100); 	//E
+
+	}
+
+	void drawNotes() {
+
+	}
+
+
 }
